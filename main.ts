34c54f674ef1276816c85423d6ef23763c30b688
Ricardomendozasosa <80003546+Ricardomendozasosa@users.noreply.github.com>
2021-03-08 19:04:50 -0400

    Restore '3/3/2021 15:52:42 Initial files for MakeCode project'

--- main.ts
+++ main.ts
@@ -5,13 +5,13 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        . . . . . f . . . . . . . . . . 
-        . . . . f 2 f f f f f f f . . . 
-        . . f f f 2 f d d d d d d f . . 
-        . f 4 4 4 2 d d d d d d d f . . 
-        . . f f f 2 f d d d d d d f . . 
-        . . . . f 2 f f f f f f f . . . 
-        . . . . . f . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . 9 9 9 6 6 6 6 6 6 6 . . . . 
+        . . 9 9 9 6 6 6 6 6 6 6 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -23,7 +23,6 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, oth
     sprite.destroy()
     otherSprite.destroy()
     info.changeScoreBy(1)
-    music.baDing.play()
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy()
@@ -34,22 +33,22 @@ let meteoro: Sprite = null
 let bala: Sprite = null
 let nave: Sprite = null
 nave = sprites.create(img`
-    . . . . . f f f f f f . . . . . 
-    . . . f f e e e e f 2 f . . . . 
-    . . f f e e e e f 2 2 2 f . . . 
-    . . f e e e f f e e e e f . . . 
-    . . f f f f e e 2 2 2 2 e f . . 
-    . . f e 2 2 2 f f f f e 2 f . . 
-    . f f f f f f f e e e f f f . . 
-    . f f e 4 4 e b f 4 4 e e f . . 
-    . f e e 4 d 4 1 f d d e f f . . 
-    . . f e e e 4 d d d d f d d f . 
-    . . . f f e e 4 e e e f b b f . 
-    . . . . f 2 2 2 4 d d e b b f . 
-    . . . . e 2 2 2 e d d e b f . . 
-    . . . . f 4 4 4 f e e f f . . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . . f f f . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . 8 8 . . . . . . . . . . . . . 
+    . 8 8 8 8 . . . . . . . . . . . 
+    . . 4 8 8 8 8 8 8 8 8 8 8 8 . . 
+    . 4 2 5 8 6 6 8 8 9 9 8 8 8 8 . 
+    . 4 2 5 8 8 8 6 6 8 8 9 9 8 8 . 
+    . . 4 8 8 8 8 8 8 8 8 8 8 8 . . 
+    . 8 8 8 8 . . . . . . . . . . . 
+    . 8 8 . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 nave.setFlag(SpriteFlag.StayInScreen, true)
 controller.moveSprite(nave, 156, 156)
@@ -57,30 +56,22 @@ info.setLife(3)
 info.setScore(0)
 game.onUpdateInterval(1000, function () {
     meteoro = sprites.create(img`
-        ........................
-        ........................
-        ........................
-        ........................
-        ..........8888..........
-        ........88111188........
-        .......8b11111188.......
-        .......81111111d8.......
-        ......8d1111111dd8......
-        ......8d111111ddd8......
-        ......8d111dddddd8......
-        ......8d1d8bddddb8......
-        ......8bdd8cdbbbc8......
-        .......811111bbc8.......
-        .......81b188888........
-        .......8b8c111b8........
-        ........881b1b88........
-        .........8b8b888.8......
-        ..........88888888......
-        ............88888.......
-        ........................
-        ........................
-        ........................
-        ........................
+        . . . . . . . c c c a c . . . . 
+        . . c c b b b a c a a a c . . . 
+        . c c a b a c b a a a b c c . . 
+        . c a b c f f f b a b b b a . . 
+        . c a c f f f 8 a b b b b b a . 
+        . c a 8 f f 8 c a b b b b b a . 
+        c c c a c c c c a b c f a b c c 
+        c c a a a c c c a c f f c b b a 
+        c c a b 6 a c c a f f c c b b a 
+        c a b c 8 6 c c a a a b b c b c 
+        c a c f f a c c a f a c c c b . 
+        c a 8 f c c b a f f c b c c c . 
+        . c b c c c c b f c a b b a c . 
+        . . a b b b b b b b b b b b c . 
+        . . . c c c c b b b b b c c . . 
+        . . . . . . . . c b b c . . . . 
         `, SpriteKind.Enemy)
     meteoro.setVelocity(-80, 0)
     meteoro.setPosition(180, randint(0, 112))
